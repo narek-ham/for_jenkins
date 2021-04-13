ec1cbc538c1b8807ae3870676721248ab1b51047
narek-ham <57501045+narek-ham@users.noreply.github.com>
2021-04-13 12:10:50 +0400

    Update before-install.bat

--- before-install.bat
+++ before-install.bat
@@ -1,1 +1,1 @@
-New-Item -Path . -Name "testfile2.txt" -ItemType "file" -Value "This is a text string2222."
+powershell.exe New-Item -Path . -Name "testfile2.txt" -ItemType "file" -Value "This is a text string2222."
